c8f1602bbabd9bab969634d4fd5e5b7e2d6e7bb4
data006 <109679683+data006@users.noreply.github.com>
2022-07-20 15:18:40 -0600

    Add files via upload

---
+++ 02-tipos de datos/tipos.py
@@ -0,0 +1,27 @@
+
+"""
+
+    c++:
+        int nombreVariable = (valor)
+        printf("el dato es %d", valor)
+
+    Python:
+        nombre_variable = (valor)15
+
+"""
+
+dato_entero = 15
+print("Dato de tipo entero: ",dato_entero)
+print(type(dato_entero))
+
+dato_entero = 3.1416
+print("Dato de tipo flotante",dato_entero)
+print(type(dato_entero))
+
+dato_entero = True
+print("Dato de tipo boleano",dato_entero)
+print(type(dato_entero))
+
+dato_entero = "Esta es una cadena de caracteres"
+print("Dato de tipo string",dato_entero)
+print(type(dato_entero))
